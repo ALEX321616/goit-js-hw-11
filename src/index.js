0e7b0e03f805177b1211d37d1e97df5fad3aad53
Alexander Tsaplev <zaplev@ukr.net>
2021-12-19 21:18:00 +0200

    1-2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,71 @@
 import './sass/main.scss';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'Simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import LoadMoreBtn from './js/load-more-btn';
+import ApiService from './js/api-service';
+import cardImgTpl from './templates/card-img.hbs';
+
+const apiService = new ApiService();
+let lightBox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionsData: 'alt',
+  doubleTapZoom: 1,
+  docClose: true,
+});
+
+const refs = {
+  searchForm: document.getElementById('search-form'),
+  imgGalleryList: document.querySelector('.gallery'),
+};
+
+const loadMoreBtn = new LoadMoreBtn({
+  selector: `[data-action= "load-more"]`,
+  hidden: true,
+});
+
+refs.searchForm.addEventListener('submit', onSearch);
+loadMoreBtn.refs.button.addEventListener('click', fetchArticles);
+
+function onSearch(e) {
+  e.preventDefault();
+
+  apiService.searchText = e.currentTarget.elements.searchQuery.value;
+  e.currentTarget.elements.searchQuery.value = '';
+  clearImgContainer();
+  loadMoreBtn.show();
+
+  apiService.resetPage();
+  if (apiService.searchText === '') {
+    loadMoreBtn.hide();
+    return Notiflix.Notify.failure('No data to search.');
+  }
+
+  fetchArticles();
+}
+
+function fetchArticles() {
+  loadMoreBtn.enable();
+  apiService
+    .fetchArticles()
+
+    .then(hits => {
+      appendImgMarkup(hits);
+      if (apiService.endPage) {
+        loadMoreBtn.hide();
+      }
+    })
+    .catch(error => loadMoreBtn.hide());
+}
+
+function appendImgMarkup(hits) {
+  refs.imgGalleryList.insertAdjacentHTML('beforeend', cardImgTpl(hits));
+  lightBox.refresh();
+}
+
+function clearImgContainer() {
+  refs.imgGalleryList.innerHTML = '';
+}
---
+++ src/js/api-service.js
@@ -0,0 +1,70 @@
+import Notiflix from 'notiflix';
+
+const options = {
+  headers: {
+    API_KEY: '24727962-734988bb0b03b5b4e85a70964',
+    BASE_URL: 'https://pixabay.com/api/',
+  },
+};
+
+export default class ApiService {
+  constructor() {
+    this.searchText = null;
+    this.pageURL = 1;
+    this.perPage = 40;
+    this.totalHits = null;
+    this.endPage = false;
+    this.safeSearch = true;
+    this.imageType = 'photo';
+    this.orientation = 'horizontal';
+  }
+
+  fetchArticles() {
+    const URL = `${options.headers.BASE_URL}?key=${options.headers.API_KEY}&q=${this.searchText}&image_type=${this.imageType}&page=${this.pageURL}&per_page=${this.perPage}&orientation=${this.orientation}&safesearch=${this.safeSearch}`;
+
+    return fetch(URL)
+      .then(response => {
+        return response.json();
+      })
+      .then(({ hits, totalHits }) => {
+        if (totalHits === 0) {
+          this.resetPage();
+          Notiflix.Notify.info(
+            'Sorry, there are no images matching your search query. Please try again.',
+          );
+          throw new Error();
+        }
+
+        if (this.pageURL === 1) {
+          Notiflix.Notify.success(`Hooray! We found ${totalHits} images!`);
+        }
+        if (this.perPage >= totalHits / this.pageURL) {
+          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+          this.endPage = true;
+        }
+
+        this.incrementPage();
+        return hits;
+      })
+      .catch(error => {
+        return error();
+      });
+  }
+  incrementPage() {
+    this.pageURL += 1;
+  }
+
+  resetPage() {
+    this.pageURL = 1;
+    this.endPage = false;
+  }
+  quantityCheck() {
+    alert();
+  }
+  get textQuery() {
+    return this.searchText;
+  }
+  set textQuery(newQuery) {
+    this.searchText = newQuery;
+  }
+}
